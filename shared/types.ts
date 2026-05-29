e2fe2a5900c6e29588dce085b8e867db7ab5fe49
uncore <contactuncor3@gmail.com>
2026-05-29 23:30:31 +0300

    feat: update project title and add vitest for testing

--- shared/types.ts
+++ shared/types.ts
@@ -1,8 +1,6 @@
 import schema from '../schema/story.schema.ts';
 import type { FromSchema } from 'json-schema-to-ts';
 
-// const const_schema =
-// schema as unknown as import('json-schema-to-ts').JSONSchema;
 export type StoryPack = FromSchema<typeof schema>;
 
 export type Page = StoryPack['pages'][number];
