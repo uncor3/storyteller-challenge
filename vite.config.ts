a10af23233b5d8393d29c7b75edb1fb9cea03f43
uncore <contactuncor3@gmail.com>
2026-05-30 00:19:38 +0300

    change vite root

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   base: '/storyteller-challenge/',
-  publicDir: 'preview/public',
+  publicDir: 'public',
+  root: 'preview',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
