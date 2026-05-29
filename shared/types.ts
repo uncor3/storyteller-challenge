afdefe43113f6644ca9e6c23099d3fd3aa37a2dd
uncore <contactuncor3@gmail.com>
2026-05-29 14:21:49 +0300

    move types

--- shared/types.ts
+++ shared/types.ts
@@ -4,3 +4,8 @@ import type { FromSchema } from 'json-schema-to-ts';
 // const const_schema =
 // schema as unknown as import('json-schema-to-ts').JSONSchema;
 export type StoryPack = FromSchema<typeof schema>;
+
+export type Page = StoryPack['pages'][number];
+export type Highlight = Extract<Page, { type: 'highlight' }>;
+export type Cover = Extract<Page, { type: 'cover' }>;
+export type Info = Extract<Page, { type: 'info' }>;
